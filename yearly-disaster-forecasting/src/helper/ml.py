68a0dd561e82f374350a3c0b081a2a0985ab4b29
Solomon Owerre <alaowerre@gmail.com>
2022-07-24 11:44:53 -0400

    reformatted codes and added tests

--- yearly-disaster-forecasting/src/helper/ml.py
+++ yearly-disaster-forecasting/src/helper/ml.py
@@ -1,26 +1,36 @@
-##################################
+####################################
 # Author: S. A. Owerre
 # Date modified: 01/05/2022
-# Class: Supervised Regression ML
-##################################
+# Class: Machine learning
+####################################
 
+# Filter warnings
 import warnings
 warnings.filterwarnings("ignore")
+
+# Data manipulation and visualization
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+# Model performance
 from sklearn.metrics import make_scorer
 from statsmodels.stats.diagnostic import acorr_ljungbox
 from sklearn.model_selection import cross_validate
 
-class RegressionModels:
-    """ This class is used for training supervised regression models."""
 
+class RegressionModels:
+    """
+    Class for training and testing supervised regression models
+    """
     def __init__(self):
-        """Parameter initialization."""
+        """
+        Parameter initialization.
+        """
         
     def custom_scorer(self):
-        """Define custom scorer currently not available in sklearn scoring list.
+        """
+        Define custom scorer currently not available in sklearn scoring list.
 
         Returns
         -------
@@ -29,20 +39,14 @@ class RegressionModels:
         wmape_scoring = make_scorer(self.wmape, greater_is_better=False)
         return wmape_scoring
         
-    def evaluate_metric_cv(
-        self, 
-        model, 
-        X_train, 
-        y_train, 
-        cv_fold, 
-        model_nm=None
-    ):
-        """Cross-validation on the training set.
+    def evaluate_metric_cv(self, model, X_train, y_train, cv_fold, model_nm=None):
+        """
+        Cross-validation on the training set.
 
         Parameters
         ----------
         model: supervised classification model
-        X_train: training feature matrix
+        X_train: feature matrix of the training set
         y_train: target variable
         cv_fold: number of cross-validation fold
 
@@ -50,12 +54,11 @@ class RegressionModels:
         -------
         Performance metrics on the cross-validation training set
         """
-        scoring = scoring = {
-            'mae':"neg_mean_absolute_error", 
-            'rmse': "neg_root_mean_squared_error",
-            'wmape':self.custom_scorer(),
-            'r2': "r2"
-        }
+        scoring = scoring = {'mae':"neg_mean_absolute_error", 
+                             'rmse': "neg_root_mean_squared_error",
+                             'wmape':self.custom_scorer(),
+                             'r2': "r2"
+                            }
         cv_results = cross_validate(model, X_train, y_train, 
         cv=cv_fold, scoring=scoring)
         mae = -cv_results["test_mae"]
@@ -63,23 +66,23 @@ class RegressionModels:
         wmape = -cv_results["test_wmape"]
         r2 =  cv_results["test_r2"]
         
-        errors = {
-            'wMAPE = {}'.format(np.round(wmape.mean(),3)), 
-            'MAE = {}'.format(np.round(mae.mean(),3)),
-            'RMSE = {}'.format(np.round(rmse.mean(),3)), 
-            'R^2 = {}'.format(np.round(r2.mean(),3))
-        }
+        errors = {'wMAPE = {}'.format(np.round(wmape.mean(),3)), 
+                  'MAE = {}'.format(np.round(mae.mean(),3)),
+                  'RMSE = {}'.format(np.round(rmse.mean(),3)), 
+                  'R^2 = {}'.format(np.round(r2.mean(),3))
+                 }
         print('5-fold cross-validation results for {}'.format(str(model_nm)))
         print('-' * 60)
         print(errors)
         print('-' * 60)
         
     def plot_wmape_svr(self, X_train, y_train, cv_fold):
-        """Plot of cross-validation wMAPE and MAE for SVR.
+        """
+        Plot of cross-validation wMAPE and MAE for SVR.
 
         Parameters
         ----------
-        X_train: training feature matrix
+        X_train: feature matrix of the training set
         y_train: target variable
         cv_fold: number of cross-validation fold
 
@@ -89,14 +92,12 @@ class RegressionModels:
         """
         C_list = [2**x for x in range(-2,11,2)]
         gamma_list = [2**x for x in range(-7,-1,2)]
-        mae_list = [
-            pd.Series(0.0, index = range(len(C_list))) 
-            for _ in range(len(gamma_list))
-        ]
-        wmape_list = [
-            pd.Series(0.0, index = range(len(C_list))) 
-            for _ in range(len(gamma_list))
-        ]
+        mae_list = [pd.Series(0.0, index = range(len(C_list))) 
+                    for _ in range(len(gamma_list))
+                   ]
+        wmape_list = [pd.Series(0.0, index = range(len(C_list))) 
+                     for _ in range(len(gamma_list))
+                    ]
 
         axes_labels = ['2^-2', '2^0', '2^2', '2^4', '2^6', '2^8', '2^10']
         gamma_labels = ['2^-7', '2^-5', '2^-3']
@@ -105,52 +106,42 @@ class RegressionModels:
 
         for i, val1 in enumerate(gamma_list):
             for j, val2 in enumerate(C_list):
-                model = SVR(C=val2, gamma = val1, kernel = 'rbf')
-                scoring = {
-                    'mae':"neg_mean_absolute_error", 
-                    'wmape':self.custom_scorer()
-                }
-                cv_results = cross_validate(
-                    model, 
-                    X_train, 
-                    y_train,
-                    cv=cv_fold, 
-                    scoring=scoring
-                )
+                model = SVR(C = val2, gamma = val1, kernel = 'rbf')
+                scoring = scoring = {'mae':"neg_mean_absolute_error", 
+                                     'wmape':self.custom_scorer()
+                                    }
+                cv_results = cross_validate(model, X_train, y_train,
+                 cv=cv_fold, scoring=scoring
+                 )
                 mae = -cv_results["test_mae"]
                 wmape = -cv_results["test_wmape"]
                 mae_list[i][j] = mae.mean()
                 wmape_list[i][j] = wmape.mean()
-            wmape_list[i].plot(
-                label="gamma="+str(gamma_labels[i]), 
-                marker="o", 
-                linestyle="-", 
-                ax=ax1
-            )
-            mae_list[i].plot(
-                label="gamma="+str(gamma_labels[i]), 
-                marker="o", 
-                linestyle="-", 
-                ax=ax2
-            )
+            wmape_list[i].plot(label = "gamma="+str(gamma_labels[i]), marker = "o", 
+                              linestyle = "-", ax = ax1
+                             )
+            mae_list[i].plot(label = "gamma="+str(gamma_labels[i]), marker = "o", 
+                             linestyle = "-", ax = ax2
+                            )
 
-        ax1.set_xlabel("C", fontsize=15)
-        ax1.set_ylabel("wMAPE", fontsize=15)
-        ax1.set_title("5-Fold Cross-Validation with RBF kernel SVR", fontsize=15)
+        ax1.set_xlabel("C", fontsize = 15)
+        ax1.set_ylabel("wMAPE", fontsize = 15)
+        ax1.set_title("5-Fold Cross-Validation with RBF kernel SVR", fontsize = 15)
         ax1.set_xticklabels(axes_labels)
         ax1.set_xticks(range(len(C_list)))
         ax1.legend(loc = 'best')
 
-        ax2.set_xlabel("C", fontsize=15)
-        ax2.set_ylabel("MAE", fontsize=15)
-        ax2.set_title("5-Fold Cross-Validation with RBF kernel SVR", fontsize=15)
+        ax2.set_xlabel("C", fontsize = 15)
+        ax2.set_ylabel("MAE", fontsize = 15)
+        ax2.set_title("5-Fold Cross-Validation with RBF kernel SVR", fontsize = 15)
         ax2.set_xticks(range(len(C_list)))
         ax2.set_xticklabels(axes_labels)
         ax2.legend(loc = 'best')
         plt.show()
 
     def evaluate_metric_test(self, y_pred, y_true, model_nm=None):
-        """Predictions on the test set.
+        """
+        Predictions on the test set.
 
         Parameters
         ----------
@@ -167,15 +158,9 @@ class RegressionModels:
         print(self.error_metrics(y_true, y_pred))
         print('-' * 60)
         
-    def diagnostic_plot(
-        self, 
-        y_true, 
-        y_pred, 
-        marker=None, 
-        color=None, 
-        label=None
-    ):
-        """Diagnostics plot.
+    def diagnostic_plot(self, y_true, y_pred, marker=None, color=None, label=None):
+        """
+        Diagnostics plot.
         
         Parameters
         ----------
@@ -189,23 +174,13 @@ class RegressionModels:
          # residuals & standardize residuals
         residual = (y_true - y_pred)
         rstandard = (residual-np.mean(residual))/np.std(residual)
-        
-        # mean absolute percentage error
-        mape = {}  
-        ape = np.abs(residual)*100/np.abs(y_true)
-        for i in range(0, len(ape), 24):
-            mape[i+24] = np.mean(ape[:i+24])
+        ape = np.abs(residual)*100/np.abs(y_true) # absolute percent error
         
         # plot figures
         plt.subplot(221)
-        plt.plot(
-            range(1,len(rstandard)+1), 
-            rstandard, 
-            color=color, 
-            marker=marker,
-            markerfacecolor='none', 
-            label=label
-        )
+        plt.plot(range(1,len(rstandard)+1), rstandard, color=color, marker=marker,
+                 markerfacecolor='none', label=label
+                )
         plt.grid(True)
         plt.xlabel('Forecast horizon (hours)')
         plt.ylabel('Standardized residuals')
@@ -213,20 +188,13 @@ class RegressionModels:
         plt.legend(loc='best')
 
         plt.subplot(222)
-        plt.plot(
-            mape.keys(), 
-            mape.values(), 
-            color=color, 
-            marker=marker, 
-            label=label
-        )
+        plt.plot(range(1,len(ape)+1), ape, color=color, marker=marker, 
+                 markerfacecolor='none', label=label
+                )
         plt.grid(True)
         plt.xlabel('Forecast horizon (hours)')
-        plt.ylabel('Mean absolute percentage error')
-        plt.title('Mean absolute percentage error over time')
-        plt.xticks(np.arange(24,121,24))
-        labels = ['0%', '40%', '80%', '120%', '160%']
-        plt.yticks(np.arange(0,170,40),labels)
+        plt.ylabel('Absolute percent error')
+        plt.title('Absolute percent error over time')
         plt.legend(loc='best')
         
         plt.subplot(223)
@@ -279,7 +247,7 @@ class RegressionModels:
         wmape = np.round(self.wmape(y_true, y_pred), 3)
         
         # plot figures
-        _, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 8))
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 8))
         ax1.scatter(y_pred, residual, color='b')
         ax1.set_xlim([-0.1, 1.1])
         ax1.set_ylim(ylim)
@@ -299,7 +267,9 @@ class RegressionModels:
         ax2.set_title('True values vs. Predicted values')
 
     def error_metrics(self, y_true, y_pred):
-        """Print out error metrics."""
+        """
+        Print out error metrics.
+        """
         wmape = self.wmape(y_true, y_pred)
         r2 = self.r_squared(y_true, y_pred)
         mae = self.mae(y_true, y_pred)
@@ -313,31 +283,41 @@ class RegressionModels:
         return errors
     
     def wmape(self, y_true, y_pred):
-        """Weighted Mean absolute percentage error."""
+        """
+        Weighted Mean absolute percentage error.
+        """
         et = (y_true - y_pred)
         wmape = np.sum(np.abs(et))*100/np.sum(np.abs(y_true))
         return wmape
 
     def mape(self, y_true, y_pred):
-        """Mean absolute percentage error."""
+        """
+        Mean absolute percentage error.
+        """
         et = (y_true - y_pred)
         mape = np.mean(np.abs(et/y_true))*100
         return mape
 
     def mae(self, y_true, y_pred):
-        """Mean absolute error."""
+        """
+        Mean absolute error.
+        """
         et = (y_true - y_pred)
         mae = np.mean(np.abs(et))
         return mae
 
     def rmse(self, y_true, y_pred):
-        """Root mean squared error."""
+        """
+        Root mean squared error.
+        """
         et = (y_true - y_pred)
         rmse = np.sqrt(np.mean(et**2))
         return rmse
 
     def r_squared(self, y_true, y_pred):
-        """r squared (coefficient of determination)."""
+        """
+        r squared (coefficient of determination).
+        """
         et = (y_true - y_pred)
         mse = np.mean(et**2)  # mean squared error
         var = np.mean((y_true - np.mean(y_true))**2)  # sample variance
--- yearly-disaster-forecasting/src/helper/transfxn.py
+++ yearly-disaster-forecasting/src/helper/transfxn.py
@@ -1,105 +1,113 @@
-#################################
+####################################
 # Author: S. A. Owerre
 # Date modified: 29/04/2022
-# Class: Transformation Pipeline
-#################################
+# Class: Transformations
+####################################
 
+# Filter warnings
 import warnings
 warnings.filterwarnings("ignore")
+
+# Data manipulation
 import numpy as np
+
+# Data pre-processing
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import OneHotEncoder
+# from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import PowerTransformer
 from sklearn.preprocessing import StandardScaler
 
 class TransformationPipeline:
-    """This class is used for transformation pipeline."""
-
+    """
+    A class for transformation pipeline
+    """
     def __init__(self):
-        """Define parameters."""
-
+        """
+        Define parameters
+        """
     def num_pipeline(self, X_train, X_test):
-        """Transformation pipeline of data with only numerical variables.
+        """
+        Transformation pipeline of data with only numerical variables
 
         Parameters
         ----------
-        X_train: training feature matrix
-        X_test: test feature matrix
+        X_train: Training feature matrix
+        X_test: Test feature matrix
 
         Returns
         -------
         Transformation pipeline and transformed data in array
         """
-        # create pipeline
-        num_pipeline = Pipeline(
-            [
-                ('p_transform', PowerTransformer(standardize=False)),
-                ('s_scaler', StandardScaler()),
-            ]
-        )
+        # Create pipeline
+        num_pipeline = Pipeline([('p_transform', PowerTransformer(standardize=False)),
+                                ('s_scaler', StandardScaler()),
+                                ])
 
-        # original numerical feature names 
+        # Original numerical feature names 
         feat_nm = list(X_train.select_dtypes('number'))
 
-        # fit transform the training set and transform the test set
+        # Fit transform the training set
         X_train_scaled = num_pipeline.fit_transform(X_train)
+        
+        # Only transform the test set
         X_test_scaled = num_pipeline.transform(X_test)
         return X_train_scaled, X_test_scaled, feat_nm
     
     def cat_encoder(self, X_train, X_test):
-        """Transformation pipeline of categorical variables.
+        """
+        Transformation pipeline of categorical variables
 
         Parameters
         ----------
-        X_train: training feature matrix
-        X_test: test feature matrix
+        X_train: Training feature matrix
+        X_test: Test feature matrix
 
         Returns
         -------
         Transformation pipeline and transformed data in array
         """
-        # instatiate class
+        # Instatiate class
         one_hot_encoder = OneHotEncoder(handle_unknown='ignore')
 
-        # fit transform the training set and only transform the test set
+        # Fit transform the training set and only transform the test set
         X_train_scaled = one_hot_encoder.fit_transform(X_train)
         X_test_scaled = one_hot_encoder.transform(X_test)
 
-        # feature names for output features
+        # Feature names for output features
         feat_list = list(X_train.select_dtypes('O'))
-        feat_nm = list(one_hot_encoder.get_feature_names_out(feat_list))
+        feat_nm = list(one_hot_encoder.get_feature_names(feat_list))
         return X_train_scaled.toarray(), X_test_scaled.toarray(), feat_nm
   
     def preprocessing(self, X_train, X_test):
-        """Transformation pipeline of data with 
-        both numerical and categorical variables.
+        """
+        Transformation pipeline of data with both numerical and categorical 
+        variables.
 
         Parameters
         ----------
-        X_train: training feature matrix
-        X_test: test feature matrix
+        X_train: Training feature matrix
+        X_test: Test feature matrix
 
         Returns
         -------
         Transformed data in array
         """
 
-        # numerical transformation pipepline
+        # Numerical transformation pipepline
         x_train = X_train.select_dtypes('number')
         x_test = X_test.select_dtypes('number')
         num_train, num_test, num_col=self.num_pipeline(x_train, x_test)
 
-        # categorical transformation pipepline
-        cat_train, cat_test, cat_col = self.cat_encoder(
-            X_train.select_dtypes('O'), 
-            X_test.select_dtypes('O')
-        )
+        # Categorical transformation pipepline
+        cat_train, cat_test, cat_col = self.cat_encoder(X_train.select_dtypes('O'), 
+                                        X_test.select_dtypes('O'))
 
-        # transformed training and test sets
+        # Transformed training and test sets
         X_train_scaled = np.concatenate((num_train,cat_train), axis = 1)
         X_test_scaled = np.concatenate((num_test,cat_test), axis = 1)
 
-        # feature names
+        # Feature names
         feat_nm = num_col + cat_col
         return X_train_scaled, X_test_scaled, feat_nm
